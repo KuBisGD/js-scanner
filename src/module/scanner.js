b8d8e94b3cb4aa268e812069ec44016b82ec03e5
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2026-09-16 16:20:00 +0200

    Add initial method heads

--- src/module/scanner.js
+++ src/module/scanner.js
@@ -10,10 +10,46 @@
  */
 class Scanner {
   /**
-   * Creates a new scanner.
+   * @type {Node.ReadableStream}
    */
-  constructor () {
-        
+  #input
+
+  /**
+   * @type {Node.WritableStream}
+   */
+  #output
+
+  /**
+   * Creates a new scanner.
+   * 
+   * @param {NodeJS.ReadableStream} input Input stream.
+   * @param {NodeJS.WritableStream} output Output stream.
+   */
+  constructor (input, output) {
+    this.#input = input
+    this.#output = output
+  }
+
+  /**
+   * Reads the next line.
+   * 
+   */
+  async nextLine () {
+    throw new Error('Not implemented')
+  }
+
+  /**
+   * Reads the next number.
+   */
+  async nextNumber () {
+    throw new Error('Not implemented')
+  }
+
+  /**
+   * Gets the next character.
+   */
+  async nextChar () {
+    throw new Error('Not implemented')
   }
 }
 
