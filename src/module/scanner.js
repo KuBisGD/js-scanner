17355657265b43f13dae9b25d850a536376c7d53
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2026-09-16 15:51:03 +0200

    init

---
+++ src/module/scanner.js
@@ -0,0 +1,20 @@
+/**
+ * Module for Scanner.
+ * 
+ * @author Edwin Johnsson ej224wy@student.lnu.se
+ * @version 1.0.0
+ */
+
+/**
+ * An input scanner.
+ */
+class Scanner {
+  /**
+   * Creates a new scanner.
+   */
+  constructor () {
+        
+  }
+}
+
+export default Scanner
