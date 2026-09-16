f069b173a68f6aa9c66e984e93e6b7dd1cedc03e
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2026-09-16 20:24:38 +0200

    Implement line reading functionality and buffer tokenization in Scanner

--- src/module/scanner.js
+++ src/module/scanner.js
@@ -5,6 +5,8 @@
  * @version 1.0.0
  */
 
+import readline from 'node:readline'
+
 /**
  * An input scanner.
  */
@@ -13,6 +15,8 @@ class Scanner {
    * @type {Node.ReadableStream}
    */
   #input
+
+  #lineReader
 
   /**
    * @type {Node.WritableStream}
@@ -31,7 +35,7 @@ class Scanner {
    * @param {NodeJS.WritableStream} output Output stream.
    */
   constructor (input, output) {
-    this.#input = input
+    this.#lineReader = new LineReader(input)
     this.#output = output
   }
 
@@ -67,35 +71,77 @@ class Scanner {
     return this
   }
 
-  async #fillBuffer () {
-    // check eof
-
-    let line = ''
-
-    try {
-      // line = await some input method
-
-    } catch {
-      // eof
-      return false
-    }
-
-    if (line === null | line === undefined) {
-      // eof
-      return false
-    }
-
+  /**
+   * 
+   * @param {string} line 
+   * @returns 
+   */
+  #tokenize (line) {
     const trimmed = line.trim()
 
-    const tokens = [] //create tokens()
-
-    if (tokens.length === 0) {
-      return await this.#fillBuffer()
+    if (trimmed.length === 0) {
+      return []
     }
+
+    const rawTokens = trimmed.split(/\s+/)
+
+    return rawTokens.filter(token => token.length > 0)
+  }
+
+  async #fillBuffer () {
+    const line = await this.#lineReader.readLine()
+
+    if (line === null) {
+      this.eof = true
+      return false
+    }
+
+    const tokens = this.#tokenize(line)
 
     this.#buffer.push(...tokens)
 
-    return true
+    return tokens.length > 0
+  }
+}
+
+class LineReader {
+  #readLine
+
+  #resolveNext = null
+
+  #queue = []
+  /**
+   * Create a new line reader.
+   * 
+   * @param {NodeJS.ReadStream} input Input method.
+   */
+  constructor (input = process.stdin) {
+    this.#readLine = readline.createInterface({ input })
+
+    this.#readLine.on('line', line => {
+      if (this.#resolveNext) {
+        this.#resolveNext(line)
+        this.#resolveNext = null
+      } else {
+        this.#queue.push(line)
+      }
+    })
+
+    this.#readLine.on('close', () => {
+      if (this.#resolveNext) {
+        this.#resolveNext(null)
+      }
+    })
+  }
+
+  async readLine () {
+    if (this.#queue.length > 0) {
+      return this.#queue.shift()
+    }
+
+    return new Promise(resolve => {
+      this.#resolveNext = resolve
+    })
   }
 }
 
