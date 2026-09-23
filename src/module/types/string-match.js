7e040ecc5036543f5fa1721545261c3352cc3c5d
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2026-09-23 12:45:24 +0200

    Add StringMatch type definition for string matching functionality

---
+++ src/module/types/string-match.js
@@ -0,0 +1,17 @@
+/**
+ * Module for a string matching type.
+ * 
+ * @author Edwin Johnsson ej224wy@student.lnu.se
+ * @version 1.0.0
+ */
+
+/**
+ * Object that defines a numeric range.
+ * 
+ * @typedef {object} StringMatch
+ * @property {RegExp} pattern Regex pattern to match a string to.
+ * @property {number} min Minimum value.
+ * @property {number} max Maximum value.
+ */
+
+export default StringMatch
